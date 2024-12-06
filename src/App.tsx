703ca586428824e508eafc35298916f683455265
winda801 <winda801@kakao.com>
2024-12-06 18:05:51 +0900

    fix: app 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-const test=()=>{alert('123')}
+  const test = () => {
+    alert("1234521");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={test}> 
+        <p onClick={test}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-        style={{background:'blue'}}
+          style={{ background: "blue" }}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
