32bc820d9779211b8e183309da76f810e0f4a410
winda801 <winda801@kakao.com>
2024-12-06 18:03:51 +0900

    fix : 리뷰반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-const test=()=>{alert('123')}
-let test123 = 123
+  const [test123, setState] = React.useState(456);
+  const test = () => {
+    if (test123 === 456) {
+      setState(123);
+    }
+    alert(test123);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
+        {test123}
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={test}> 
+        <p onClick={test}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-        style={{background:'blue'}}
+          style={{ background: "blue" }}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
